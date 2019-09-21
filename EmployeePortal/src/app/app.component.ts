20fb88418660bddefc71e77452d641c948651c29
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2019-09-21 16:04:11 -0500

    two way data binding in angular with ngModel

--- EmployeePortal/src/app/app.component.ts
+++ EmployeePortal/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
 
     selector: 'my-app',
-    template: `<my-employee></my-employee>`
+    template: `
+Name: <input [(ngModel)]='name'/>
+<br/>
+You entered: {{name}}
+
+`
 })
 
 
 
 export class AppComponent {
+    name: string = 'Tom';
+
 
     onClick(): void {
 
