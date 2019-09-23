b1af34a3914fd22d68636afd6bba0c22a787c891
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2019-09-23 10:35:32 -0500

    delete interface export class and constructor

--- EmployeePortal/src/app/employee/employee.js
+++ EmployeePortal/src/app/employee/employee.js
@@ -1,16 +1,2 @@
 "use strict";
-var Employee = (function () {
-    function Employee(code, name, gender, annualSalary, dateOfBirth) {
-        this.code = code;
-        this.name = name;
-        this.gender = gender;
-        this.annualSalary = annualSalary;
-        this.dateOfBirth = dateOfBirth;
-    }
-    Employee.prototype.computeMonthlySalary = function (annualSalary) {
-        return annualSalary / 12;
-    };
-    return Employee;
-}());
-exports.Employee = Employee;
 //# sourceMappingURL=employee.js.map
